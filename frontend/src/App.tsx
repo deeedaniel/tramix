42517d353dbeeef7821b11744345bcc2950e6ec1
Daniel Nguyen <nguyendaniel1312@gmail.com>
2026-02-27 01:36:35 -0800

    remove rel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,11 +26,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <a
-        href="https://getsongbpm.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://getsongbpm.com/" target="_blank">
         GetSongBPM
       </a>
     </>
