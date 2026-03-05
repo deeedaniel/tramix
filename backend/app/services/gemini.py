f6570ffec34ff2396881261b607e119e6533742b
Daniel Nguyen <nguyendaniel1312@gmail.com>
2026-03-05 14:56:47 -0800

    create routing and reset css

--- backend/app/services/gemini.py
+++ backend/app/services/gemini.py
@@ -7,5 +7,7 @@ def get_song_suggestions(string: str) -> str:
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
         contents=f"Suggest 5 songs that have similar BPMs and are in the same key as: {string}. Return the songs in a JSON array of objects with the following properties: name, artist, bpm, key. Only return the JSON array, no other text."
+
+        
     )
     return response.text
