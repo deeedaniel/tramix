8f66c4863ed2c8f8c9b109fcab6c69b545381955
Daniel Nguyen <nguyendaniel1312@gmail.com>
2026-04-13 01:33:35 -0700

    reduce song search to 10 songs and change to faster model

--- backend/app/services/gemini.py
+++ backend/app/services/gemini.py
@@ -6,8 +6,12 @@ client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 async def get_song_suggestions(string):
 
+    # for model in client.models.list():
+    #     print('hello')
+    #     print(model.name)
+
     research_query = (
-        f"Search for 20 songs similar in genre, mood, and production style to '{string}'. "
+        f"Search for 10 songs similar in genre, mood, and production style to '{string}'. "
         "For each song, provide the Name, Artist, Musical Key, and the 'Intended BPM'. "
         "GUIDELINES TO PREVENT HALLUCINATION: "
         "1. VERIFY BPM: Cross-reference Musicnotes.com or official sheet music to find the 'Project Tempo'. "
@@ -19,7 +23,7 @@ async def get_song_suggestions(string):
     )
 
     research_response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-2.5-flash-lite",
         contents=research_query,
         config=types.GenerateContentConfig(
             tools=[types.Tool(google_search=types.GoogleSearch())]
@@ -43,10 +47,11 @@ async def get_song_suggestions(string):
         }
     }
 
+
     formatting_prompt = f"Convert this song list into a JSON array based on the schema: {raw_text}"
 
     final_response = client.models.generate_content(
-        model="gemini-2.5-flash", 
+        model="gemini-2.5-flash-lite", 
         contents=formatting_prompt,
         config=types.GenerateContentConfig(
             system_instruction="Return ONLY the JSON array. Do not include markdown or backticks.",
